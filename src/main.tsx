7c48b3bd90a5ac8aa7093c18ec3b61e37187592c
Soustin0502 <75117852+Soustin0502@users.noreply.github.com>
2025-06-11 15:42:28 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { HashRouter } from 'react-router-dom'
 import { ThemeProvider } from './contexts/ThemeContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// Create a client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, // 1 minute
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
-      <ThemeProvider>
-        <App />
-      </ThemeProvider>
-    </HashRouter>
+    <QueryClientProvider client={queryClient}>
+      <HashRouter>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </HashRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
